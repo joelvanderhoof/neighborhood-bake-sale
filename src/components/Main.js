7730bbbedd0b7263f91da1af110fd15908433a0f
Randy <randylam87@gmail.com>
2017-08-18 15:53:23 -0700

    small style changes, wraped main in a div to allow nav to render as container fluid.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './Nav';
 import SearchMap from './children/SearchMap';
-import Store from './children/Store';
+import Store from './Store';
 import SellerAdmin from './SellerAdmin'
 
 export default class Main extends Component {
@@ -13,11 +13,13 @@ export default class Main extends Component {
     render() {
         return (
             <Router>
-                <div className='container'>
+                <div className='main'>
                     <Nav />
-                    <Route  path='/search-map' component={SearchMap} />
-                    <Route  path='/store' component={Store} />
-                    <Route path='/selleradmin' component={SellerAdmin} />
+                    <div className='container'>
+                        <Route  path='/search-map' component={SearchMap} />
+                        <Route  path='/store' component={Store} />
+                        <Route path='/selleradmin' component={SellerAdmin} />
+                    </div>
                 </div>
             </Router>
         )
