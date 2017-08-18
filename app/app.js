dea2a6642617b4ec6da0feacf8e7f828b6a7851e
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-08-17 23:45:44 -0700

    cleaned up file structure

--- app/app.js
+++
@@ -1,9 +0,0 @@
-// React Dependencies
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Components
-import Main from './components/Main.js';
-
-// Renders Main component
-ReactDOM.render(<Main />, document.getElementById('app') );
