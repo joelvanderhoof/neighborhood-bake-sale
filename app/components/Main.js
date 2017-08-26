2e79cbaed678e63ddcd2d1cbdf528768c505041e
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-08-26 12:07:53 -0700

    update for heroku deployment

---
+++ app/components/Main.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Nav from './Nav'
+import SearchMap from './children/SearchMap'
+import SellerAdmin from './SellerAdmin'
+import Store from './children/Store'
+
+class Main extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <Router>
+                <div className='container'>
+                    <Nav />
+                    <Route path='/search-map' component={SearchMap} />
+                    <Route path='/store' component={Store} />
+                    <Route path='/selleradmin' component={SellerAdmin} />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default Main;
---
+++ models/Orders.js
@@ -0,0 +1,21 @@
+// const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+
+// var CustomerSchema = new Schema();
+
+// var Customer = mongoose.model("Customer", CustomerSchema);
+
+// module.exports = Customer;
+
+// {
+//     customerID: String, //same as the ID from the Customer collection
+//     sellerID: String, //same as the ID from the Seller collection
+//     order: Array, //Array of menu items
+//     total: Number, //Order sales amount
+//     status: String, //The status will be set to specific strings by specific functions
+//     // To check status compare the strings
+    
+
+    
+// }
