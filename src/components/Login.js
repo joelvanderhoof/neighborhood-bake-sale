1e5ea82dfa8c75e1f4b58392ae1033a9332af07c
Randy <randylam87@gmail.com>
2017-08-29 00:24:36 -0700

    added saveState functions to signup and login. state is now passable and is console logged for now. next step is passport

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import LoginForm from './Login/LoginForm';
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+    this.saveState = this.saveState.bind(this);
+  }
+
+  saveState (currentState) {
+    console.log(currentState);
+  }
+
   render () {
     return (
       <div className='container'>
         <div className='col-6'>
-          <LoginForm />
+          <LoginForm saveState={this.saveState}/>
         </div>
       </div>
     )
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import SignUpForm from './SignUp/SignUpForm';
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+    this.saveState = this.saveState.bind(this);
+  }
+
+  saveState (currentState) {
+    console.log(currentState);
+  }
+
   render () {
     return (
       <div className='container'>
         <div className='col-6'>
-          <SignUpForm />
+          <SignUpForm saveState={this.saveState}/>
         </div>
       </div>
     )
