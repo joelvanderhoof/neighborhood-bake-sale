c79fef8687e98910b6221c97f6940aff95a1c13c
Eve Flores <berryknotty@hotmail.com>
2017-08-28 21:55:10 -0700

    background fixed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Search from './Homepage/Search';
 
-
 class Home extends Component {
   render () {
     return (
-      <div className='container border container-store'>
-        <img
-            className='img-fluid rounded'
-            src='http://images.huffingtonpost.com/2015-03-27-1427496203-1352796-bigstockWoodenspoonandingredientso76325474.jpg'
-            alt='Home Image'/>
-        <h1>
-        Foodies
-        </h1>
+      <div 
+        style={{backgroundImage:"url('https://s-media-cache-ak0.pinimg.com/originals/36/62/29/366229e72efbb7aadf3c8361864d6c5a.jpg')",
+                width: "100%",
+                height: "600px",
+                backgroundPosition: "center", 
+                backgroundSize: "cover"}}> 
+        {/* <img className='img-fluid rounded'
+            src='https://s-media-cache-ak0.pinimg.com/originals/36/62/29/366229e72efbb7aadf3c8361864d6c5a.jpg'
+            alt='Home Image'/>    */}
+        <h1 className="text-center">Foodies</h1>
+        {/* Name of the App- still being decided "Foodies" */}
       <Search />
       </div>
     );
