d680aa48f710c0bd8cc20e45c817f27405f11be0
dennis-nguyen <nguyen.denn@gmail.com>
2017-08-31 19:50:23 -0700

    Updated Messages feature

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const CustomerSchema = new Schema({
+const OrderSchema = new Schema({
     customerID: String, //same as the ID from the Customer model
     sellerID: String, //same as the ID from the Seller collection
-    order: [{
-            type: Schema.Types.ObjectId, 
-            ref: 'MenuItem'}], //Array of menu item IDs
-    total: Number, //Order sales amount
+    customerName: String,
+    storeName: String,
+    order: String, //Menu Item Name
     status: String, //The status will be set to specific strings by specific functions
     // To check status compare the strings
     
 });
 
-const Customer = mongoose.model("Customer", CustomerSchema);
+const Order = mongoose.model("Order", OrderSchema);
 
-module.exports = Customer;
+module.exports = Order;
