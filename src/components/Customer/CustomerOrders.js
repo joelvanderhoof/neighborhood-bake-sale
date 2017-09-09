2c1c929d352e953f5409c9b83d3f728fd7445394
Tri Ton <triton@MacBook-Pro.local>
2017-09-08 18:41:47 -0700

    updates

--- src/components/Customer/CustomerOrders.js
+++ src/components/Customer/CustomerOrders.js
@@ -3,9 +3,10 @@ import React from 'react';
 const CustomerOrders = (props) => {
 
     const {buyerFirstName, buyerLastName, customerId, date, items, orderTotal, pickedUp, sellerFirstName, sellerId, sellerLastName, status, storeId} = props.item;
+    
     items.map(data => {
-        console.log('Items: ', data.item);
-        console.log('Price:', data.price);
+        console.log('Item:', data.item);
+        console.log('Price: ', data.price);
     });
     // Render li tag with props.children 
     return (
