7c4380f5f21ebdf9d2349628b47ae56cadd275d7
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2017-09-09 06:29:33 -0700

    Search page now gets geocoordinates from google for search location.  The Search map is centered on that lat/lng.  The city from the search address is extracted via regex.  All stores in the db with a matching searchCity value are returned by the /store-marker api route and populated on the map.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Search from './Homepage/Search';
 import Image from './Homepage/Image';
+import MapSearch from './MapSearch/MapSearch';
+import axios from 'axios';
+
 
 class Home extends Component {
   constructor(props) {
@@ -8,23 +11,52 @@ class Home extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.extractSearchCity = this.extractSearchCity.bind(this);
+    this.getStartLatLng = this.getStartLatLng.bind(this);
 
     this.state = {
+      searchCity: '',
       secretData: '',
-      value: ''
+      value: '',
+      searchButtonClicked: false
     };
   }
 
-  componentDidMount() {
+  extractSearchCity(text) {
+    let match = "";
+    let result = text.match(/\,(.*)\,/);
+
+    if (result) {
+      match = result[1].trim();
+    };
+
+    console.log(`extracted city: ${match}`);    
+    this.setState({ searchCity: match });
+  }
+
+  getStartLatLng(address) {
+    //URL encode address
+    encodeURI(address);
+    return new Promise((resolve, reject) => {
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}`)
+      .then((res) => {
+        let latLng = res.data.results[0].geometry.location;
+        resolve(latLng);
+      })
+    })
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    console.log(event.target.value);
+    this.setState({ value: event.target.value.substring(0, 140) });
   }
 
-  handleSubmit(event) {
-    alert(`A location was submitted: ${this.state.value}`);
-    event.preventDefualt();
+  async handleSubmit(event) {
+    this.extractSearchCity(this.state.value);
+    let latLng = await this.getStartLatLng(this.state.value);
+    this.setState({ latLng });
+    this.setState({ searchButtonClicked: true })
+    
   }
 
   render () {
@@ -36,11 +68,18 @@ class Home extends Component {
             fontFamily: "Lobster Two, cursive",
             fontSize: 200}}>Foodies</h1>
         {/* Name of the App- still being decided "Foodies" */}
-      <Search 
-        value={this.state.value}
-        onChangeValue={this.handleChange}
-        onSubmit={this.handleSubmit}
-      />
+      {this.state.searchButtonClicked ?  (
+        <MapSearch 
+          latLng={this.state.latLng}
+          searchCity={this.state.searchCity}
+        />
+      ) : (
+        <Search 
+          value={this.state.value}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+      )}
       <Image />
       </div>
     );
